62ea8da1730af2a800998a8b4dd57021c73ebc9f
33504 <feng.bie@myt11.com>
2020-06-22 14:59:35 +0800

    modify login.html

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, login_required, logout_user
+
 from . import auth
-from ..models import User
 from .forms import LoginForm
+from ..models import User
 
 
 @auth.route('/login', methods=['GET', 'POST'])
