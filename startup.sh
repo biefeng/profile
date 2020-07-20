81ff85919eaa2d59b000b90eef50fbcb7eba4dd3
33504 <feng.bie@myt11.com>
2020-07-20 17:52:16 +0800

    modify the startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 source /home/root/program/miniconda2/bin/activate blog_mini
 
-cd /home/root/program/Blog_mini
+cd /home/root/program/profile
 
 nohup gunicorn -b 0.0.0.0:80 --access-logfile=logs/access.out  --access-logformat='{"remote_ip":"%(h)s","request_id":"%({X-Request-Id}i)s","response_code":"%(s)s","request_method":"%(m)s","request_path":"%(U)s","request_querystring":"%(q)s","request_timetaken":"%(D)s","response_length":"%(B)s"}' -p logs/pid  manage:app  > logs/log.out 2>&1 & 
 
