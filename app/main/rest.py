0b6bc13bd360584cfc201a851b6f8b87e2083590
BieFeNg <feng.bie@myt11.com>
2020-09-29 17:54:01 +0800

    add avatar url

--- app/main/rest.py
+++ app/main/rest.py
@@ -2,6 +2,7 @@
 # author : BieFeNg
 # date_time 2020/07/07 10:17
 # file_name : rest.py
+import hashlib
 
 from flask import request, current_app, render_template, Response
 from jsonpickle import pickler
@@ -26,3 +27,16 @@ def check_token():
     if authenticated_user.is_auth():
         return jsonify({"data": 1})
     return jsonify({"data": 0})
+
+
+@main.route("/avatar", methods=["GET"])
+def gravatar(size=40, default='identicon', rating='g'):
+    # if request.is_secure:
+    #     url = 'https://secure.gravatar.com/avatar'
+    # else:
+    #     url = 'http://www.gravatar.com/avatar'
+    size = request.args.get("size", 18)
+    url = 'https://gravatar.loli.net/avatar'
+    _hash = hashlib.md5("BieFeNg".encode('utf-8')).hexdigest()
+    return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+        url=url, hash=_hash, size=size, default=default, rating=rating)
