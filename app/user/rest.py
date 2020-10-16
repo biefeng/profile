bc48af8f2f8128bc504e3e0c4b93d06f128f93b1
BieFeNg <feng.bie@myt11.com>
2020-10-16 15:24:21 +0800

    handle the github api error

--- app/user/rest.py
+++ app/user/rest.py
@@ -9,6 +9,7 @@ from flask_jwt import jwt_required
 from app.models import User
 import requests
 from . import user
+import time
 
 
 def gravatar(h, size=40):
@@ -40,14 +41,36 @@ def github_access_token():
     _state = json_data.get("state")
 
     if _code is not None:
-        res = requests.post("https://github.com/login/oauth/access_token", headers={"Accept": "application/json"},
-                            data={"code": _code, "state": _state, "client_id": "b1fab3539af78d4ad4b5",
-                                  "client_secret": "71dcffc193caab6d12d986dd9d78515c744d32fd"})
-        res_json = res.json()
-        _access_token = res_json.get("access_token")
-        if _access_token is not None:
-            res = requests.get("https://api.github.com/user", headers={"Authorization": "token " + _access_token,
-                                                                       "Accept": "application/vnd.github.v3+json",
-                                                                       'Connection': 'close'}, verify=False)
-            return res.json()
-        return res_json
+        limit = 4
+        for o in limit:
+            try:
+                res = requests.post("https://github.com/login/oauth/access_token",
+                                    headers={"Accept": "application/json"},
+                                    data={"code": _code, "state": _state, "client_id": "b1fab3539af78d4ad4b5",
+                                          "client_secret": "71dcffc193caab6d12d986dd9d78515c744d32fd"})
+
+                res_json = res.json()
+                _access_token = res_json.get("access_token")
+            except requests.exceptions.ConnectionErrora as e:
+                if o < (limit - 1):
+                    time.sleep(0.5)
+
+            else:
+                time.sleep(0.1)
+                break
+
+        for o in limit:
+            try:
+                if _access_token is not None:
+                    res = requests.get("https://api.github.com/user",
+                                       headers={"Authorization": "token " + _access_token,
+                                                "Accept": "application/vnd.github.v3+json",
+                                                'Connection': 'close'}, verify=False)
+                    return res.json()
+            except requests.exceptions.ConnectionErrora as e:
+                if o < (limit - 1):
+                    time.sleep(0.5)
+
+            else:
+                time.sleep(0.1)
+                break
